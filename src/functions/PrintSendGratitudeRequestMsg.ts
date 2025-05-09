66fee363aec6fdee5fd2dcb21f2ab897df3ea1d5
nicolas castellano <nicolas.castellano.6@hotmail.com>
2025-05-09 12:27:01 -0300

    added a timer trigger asking the user through a text file what are 3 things to be grateful for

--- src/functions/PrintSendGratitudeRequestMsg.ts
+++ src/functions/PrintSendGratitudeRequestMsg.ts
@@ -1,9 +1,15 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 //  Module to write to file, this will be replaced later for a sms msg broker module
-import { writeFileSync } from "fs";
+import { writeFileSync, readFileSync, appendFileSync } from "fs";
+
 
 export async function PrintSendGratitudeRequestMsg(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Timer function processed request.');
+    const fileLocation = "src/models/responseStack.txt";
+    const dateNow = new Date().toString();
+    const morningGratitudeRequestMsgText = `Good morning champ! Hope you slept well. It’s your wellness pal reminding you to name 3 things you are thankful for today. -> Sent at ${dateNow} \n`;
+    
+    // Append the request to the file. This will be replaced with a SMS to the user phone later on
+    appendFileSync(fileLocation, morningGratitudeRequestMsgText);
 }
 
 app.timer('PrintSendGratitudeRequestMsg', {
