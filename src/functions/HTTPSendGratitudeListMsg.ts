80367b952d5fd0dbeb1d7caad147f19c62f20407
nicolas castellano <nicolas.castellano.6@hotmail.com>
2025-05-09 15:32:51 -0300

    stripped away unused lines

--- src/functions/HTTPSendGratitudeListMsg.ts
+++ src/functions/HTTPSendGratitudeListMsg.ts
@@ -1,4 +1,4 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, Exception, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
 import { writeFileSync, readFileSync, appendFileSync } from "fs";
 
@@ -6,11 +6,6 @@ import constantsJson from "./../data/constants.json";
 import { formatMsgWithDate } from "../utils/formatters";
 
 export async function HTTPSendGratitudeListMsg(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    // context.log(`Http function processed request for url "${request.url}"`);
-
-    // const name = request.query.get('name') || await request.text() || 'world';
-
-    // return { body: `Hello, ${name}!` };
 
     if (request.method === "GET")
         return {body: "Hello message GET"}
@@ -20,17 +15,21 @@ export async function HTTPSendGratitudeListMsg(request: HttpRequest, context: In
         const requestFormData = await request.json().catch((reason) => {
             return {status: 400, body: "Please enter the data"}
         });
+        try {
 
-        if (typeof requestFormData["answers"] !== "object") return {status: 400, body: "Invalid gratitude format, must use array under the json key 'answer'"}
-
-        const answersArray:Array<string> = Array.from(requestFormData["answers"]) ?? [];
-        if (answersArray.length !== 3) return {status: 400, body: "Please enter the 3 gratitudes"}
-
-        const formattedUserResponseMsgRecord: string = formatMsgWithDate(constantsJson.userName, `My 3 gratitudes are: ${answersArray.join(", ")}`);
-        const formattedMachineResponseMsgRecord: string = formatMsgWithDate(constantsJson.machineName, constantsJson.gratitudesSubmissionSuccessMsg);
-
-        appendFileSync(constantsJson.txtFileResponseStack, formattedUserResponseMsgRecord);
-        appendFileSync(constantsJson.txtFileResponseStack, formattedMachineResponseMsgRecord);
+            if (typeof requestFormData["answers"] !== "object") return {status: 400, body: "Invalid gratitude format, must use array under the json key 'answer'"}
+            
+            const answersArray:Array<string> = Array.from(requestFormData["answers"]) ?? [];
+            if (answersArray.length !== 3) return {status: 400, body: "Please enter the 3 gratitudes"}
+            
+            const formattedUserResponseMsgRecord: string = formatMsgWithDate(constantsJson.userName, `My 3 gratitudes are: ${answersArray.join(", ")}`);
+            const formattedMachineResponseMsgRecord: string = formatMsgWithDate(constantsJson.machineName, constantsJson.gratitudesSubmissionSuccessMsg);
+            
+            appendFileSync(constantsJson.txtFileResponseStack, formattedUserResponseMsgRecord);
+            appendFileSync(constantsJson.txtFileResponseStack, formattedMachineResponseMsgRecord);
+        } catch (e) {
+            return {status: 500, body: "Something occurred: " + e.message}
+        }
 
 
         return {body: "Submission successful"}
